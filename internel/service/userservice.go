8351ea3b47cd13cedcf0a4267fead27de430d825
ArifulProtik <mdarifulislam.protik@gmail.com>
2023-04-01 00:00:42 +0600

    Added Hashing

--- internel/service/userservice.go
+++ internel/service/userservice.go
@@ -7,8 +7,12 @@ import (
 )
 
 func (s *Service) SaveUser(usr *utility.UserInput) (*ent.User, error) {
+	newPass, err := utility.HashBeforeSave(usr.Password)
+	if err != nil {
+		return nil, err
+	}
 	newUser, err := s.client.User.Create().SetName(usr.Name).
-		SetUsername(usr.Username).SetEmail(usr.Email).SetPassword(usr.Password).Save(context.Background())
+		SetUsername(usr.Username).SetEmail(usr.Email).SetPassword(string(newPass)).Save(context.Background())
 	if err != nil {
 		s.log.Debug(err)
 		return nil, err
